dd4e78f7d9299935732d217e1152c5f244c7df16
Gabriel Santiago Pujol Fariña <gabrielpujol1987@users.noreply.github.com>
2026-02-13 21:35:44 -0500

    First commit, these files were generated as part of the project.

---
+++ src/test/kotlin/com/gabriel/endpointops/EndpointopsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gabriel.endpointops
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EndpointopsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
