e5e497c48e329daac999b0bf1e811ea53683ede8
Gabriel Santiago Pujol Fariña <gabrielpujol1987@users.noreply.github.com>
2026-02-16 17:00:14 -0500

    Added 'endpointops' to 'application.yml', and a decorator to the 'EndpointopsApplication' in preparation for the different pagination strategies.

--- src/main/kotlin/com/gabriel/endpointops/EndpointopsApplication.kt
+++ src/main/kotlin/com/gabriel/endpointops/EndpointopsApplication.kt
@@ -1,8 +1,10 @@
 package com.gabriel.endpointops
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 class EndpointopsApplication
 
