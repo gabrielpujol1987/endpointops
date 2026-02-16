b2525546b0e8e97f2c302eae767a2718bfc898b8
Gabriel Santiago Pujol Fariña <gabrielpujol1987@users.noreply.github.com>
2026-02-16 11:28:08 -0500

    Added the 'findTop50ByDeviceIdOrderByCreatedAtDesc' function to the 'DeviceEventRepository' and it's magically implemented???

--- src/main/kotlin/com/gabriel/endpointops/repo/DeviceEventRepository.kt
+++ src/main/kotlin/com/gabriel/endpointops/repo/DeviceEventRepository.kt
@@ -4,4 +4,7 @@ import com.gabriel.endpointops.domain.DeviceEvent
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
-interface DeviceEventRepository : JpaRepository<DeviceEvent, UUID>
+interface DeviceEventRepository : JpaRepository<DeviceEvent, UUID> {
+    fun findTop50ByDeviceIdOrderByCreatedAtDesc(deviceId: String): List<DeviceEvent>
+
+}
